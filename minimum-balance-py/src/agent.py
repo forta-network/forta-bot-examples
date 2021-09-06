077f6f0efaa8ad0781278a800a81571983795b96
Haseeb Rabbani <haseeb.rabbani@gmail.com>
2021-09-06 17:28:04 -0400

    min balance python agent tests

--- minimum-balance-py/src/agent.py
+++ minimum-balance-py/src/agent.py
@@ -4,26 +4,36 @@ from forta_agent import Finding, FindingType, FindingSeverity, get_json_rpc_url
 ACCOUNT = "0x6efef34e81fd201edf18c7902948168e9ebb88ae"
 MIN_BALANCE = int("500000000000000000")  # 0.5 eth
 
+
+def provide_handle_block(w3):
+    def handle_block(block_event):
+        findings = []
+
+        balance = int(w3.eth.get_balance(
+            Web3.toChecksumAddress(ACCOUNT), int(block_event.block_number)))
+        print(f'balance is {balance}')
+        if (balance >= MIN_BALANCE):
+            return findings
+
+        findings.append(Finding({
+            'name': "Minimum Account Balance",
+            'description': f'Account balance ({balance}) below threshold ({MIN_BALANCE})',
+            'alert_id': "FORTA-6",
+            'severity': FindingSeverity.Info,
+            'type': FindingType.Suspicious,
+            'metadata': {
+                'account': ACCOUNT,
+                'balance': balance
+            }
+        }))
+        return findings
+
+    return handle_block
+
+
 w3 = Web3(Web3.HTTPProvider(get_json_rpc_url()))
+real_handle_block = provide_handle_block(w3)
 
 
 def handle_block(block_event):
-    findings = []
-
-    balance = int(w3.eth.get_balance(
-        Web3.toChecksumAddress(ACCOUNT), int(block_event.block_number)))
-    if (balance >= MIN_BALANCE):
-        return findings
-
-    findings.append(Finding({
-        'name': "Minimum Account Balance",
-        'description': f'Account balance ({balance}) below threshold ({MIN_BALANCE})',
-        'alert_id': "FORTA-6",
-        'severity': FindingSeverity.Info,
-        'type': FindingType.Suspicious,
-        'metadata': {
-            'account': ACCOUNT,
-            'balance': balance
-        }
-    }))
-    return findings
+    return real_handle_block(block_event)
---
+++ minimum-balance-py/src/agent_test.py
@@ -0,0 +1,39 @@
+from unittest.mock import Mock
+from web3 import Web3
+from forta_agent import FindingSeverity, FindingType, create_block_event
+from agent import provide_handle_block, ACCOUNT, MIN_BALANCE
+
+mock_web3 = Mock(eth=Mock(get_balance=Mock()))
+handle_block = provide_handle_block(mock_web3)
+block_event = create_block_event(
+    {'block_hash': '0xa', 'block_number': 1})
+
+
+class TestMinBalanceAgent:
+    def test_returns_empty_findings_if_balance_above_threshold(self):
+        mock_web3.eth.get_balance.return_value = '500000000000000001'
+
+        findings = handle_block(block_event)
+
+        assert len(findings) == 0
+        mock_web3.eth.get_balance.assert_called_once_with(
+            Web3.toChecksumAddress(ACCOUNT), int(block_event.block_number))
+
+    def test_returns_finding_if_balance_below_threshold(self):
+        mock_web3.eth.get_balance.reset_mock()
+        balance = '1'
+        mock_web3.eth.get_balance.return_value = balance
+
+        findings = handle_block(block_event)
+
+        assert len(findings) == 1
+        mock_web3.eth.get_balance.assert_called_once_with(
+            Web3.toChecksumAddress(ACCOUNT), int(block_event.block_number))
+        finding = findings[0]
+        assert finding.name == "Minimum Account Balance"
+        assert finding.description == f'Account balance ({balance}) below threshold ({MIN_BALANCE})'
+        assert finding.alert_id == "FORTA-6"
+        assert finding.severity == FindingSeverity.Info
+        assert finding.type == FindingType.Suspicious
+        assert finding.metadata['account'] == ACCOUNT
+        assert finding.metadata['balance'] == int(balance)
