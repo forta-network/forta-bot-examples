9829cd5b972f8c0d80f3c3f47ed7c86d6f1c88eb
Haseeb Rabbani <haseeb.rabbani@gmail.com>
2022-04-06 15:11:07 +0400

    update high gas py example to 0.1.1

--- high-gas-py/src/agent.py
+++ high-gas-py/src/agent.py
@@ -1,4 +1,4 @@
-from forta_agent import Finding, FindingType, FindingSeverity
+from forta_agent import Finding, FindingType, FindingSeverity, get_transaction_receipt
 
 MEDIUM_GAS_THRESHOLD = 1000000
 HIGH_GAS_THRESHOLD = 3000000
@@ -7,30 +7,34 @@ CRITICAL_GAS_THRESHOLD = 7000000
 findings_count = 0
 
 
-def handle_transaction(transaction_event):
-    # limiting this agent to emit only 5 findings so that the alert feed is not spammed
-    global findings_count
-    if findings_count >= 5:
-        return []
+def provide_handle_transaction(get_transaction_receipt):
+    def handle_transaction(transaction_event):
+        # limiting this agent to emit only 5 findings so that the alert feed is not spammed
+        global findings_count
+        if findings_count >= 5:
+            return []
 
-    findings = []
+        findings = []
 
-    gas_used = int(transaction_event.gas_used)
-    if gas_used < MEDIUM_GAS_THRESHOLD:
+        receipt = get_transaction_receipt(transaction_event.hash)
+        gas_used = int(receipt.gas_used)
+        if gas_used < MEDIUM_GAS_THRESHOLD:
+            return findings
+
+        findings.append(Finding({
+            'name': 'High Gas Used',
+            'description': f'Gas Used: {gas_used}',
+            'alert_id': 'FORTA-1',
+            'type': FindingType.Suspicious,
+            'severity': get_severity(gas_used),
+            'metadata': {
+                'gas_used': gas_used
+            }
+        }))
+        findings_count += len(findings)
         return findings
 
-    findings.append(Finding({
-        'name': 'High Gas Used',
-        'description': f'Gas Used: {gas_used}',
-        'alert_id': 'FORTA-1',
-        'type': FindingType.Suspicious,
-        'severity': get_severity(gas_used),
-        'metadata': {
-            'gas_used': gas_used
-        }
-    }))
-    findings_count += len(findings)
-    return findings
+    return handle_transaction
 
 
 def get_severity(gas_used):
@@ -40,3 +44,10 @@ def get_severity(gas_used):
         return FindingSeverity.High
     else:
         return FindingSeverity.Medium
+
+
+real_handle_transaction = provide_handle_transaction(get_transaction_receipt)
+
+
+def handle_transaction(transaction_event):
+    return real_handle_transaction(transaction_event)
